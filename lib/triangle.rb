a7055ced1b0a89cff2977c509123bebc6a0b4d19
Christian Pardo <chrispardopersonal@gmail.com>
2020-05-03 22:37:48 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  
+  attr_reader :a, :b, :c 
+  
+  def initialize(a, b, c)
+    @a = a 
+    @b = b 
+    @c = c 
+  end
+  
+  def kind 
+    
+    
+  end 
+  
+  class TriangleError < StandardError
+    
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
+    
+  end   
+  
 end
